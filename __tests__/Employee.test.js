2ad5744d89bdc01ee77c43930d688d928a655efd
VSartz <elsartz@gmail.com>
2022-04-24 22:40:32 -0400

    employee pass tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,10 +1,35 @@
 const Employee = require('../lib/Employee.js');
-jest.mock('../lib/Employee.js');
 
 test('should return a name, a number and an email', () => {
     const employee = new Employee('Name');
 
     expect(employee.name).toBe('Name');
+    employee.id = 1;
     expect(employee.id).toEqual(expect.any(Number));
+    employee.email = 'email@example.com';
     expect(employee.email).toEqual(expect.any(String));
+});
+
+test('should get a name', () => {
+    const employee = new Employee('Name');
+
+    expect(employee.getName()).toEqual(expect.any(String));
+});
+
+test('should get a number as an Id', () => {
+    const employee = new Employee('Name');
+
+    expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+test('should get an email', () => {
+    const employee = new Employee('Name');
+
+    expect(employee.getEmail()).toEqual(expect.any(String));
+});
+
+test('should get a role', () => {
+    const employee = new Employee('Name');
+
+    expect(employee.getRole()).toEqual('Employee');
 });
